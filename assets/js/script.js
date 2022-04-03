829ce58608fd622e925006107adacb28c0a6c8ea
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-04-03 14:21:44 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,7 +162,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -183,6 +183,12 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+  setInterval(function(){
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  }, (1000 * 60) * 30)
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -198,16 +204,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    //console.log("activate", this);
+    $(this).addClass('dropover')
+    $('.bottom-trash').addClass('bottom-trash-drag')
   },
   deactivate: function(event) {
-    //console.log("deactivate", this);
+    $(this).removeClass('dropover')
+    $('.bottom-trash').removeClass('bottom-trash-drag')
   },
   over: function(event) {
-    //console.log("over", event.target);
+    $(event.target).addClass('dropover-active')
   },
   out: function(event) {
-    //console.log("out", event.target);
+    $(event.target).removeClass('dropover-active')
   },
   update: function(event) {
       // array to store the task data in
@@ -250,10 +258,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log("over");
+    $('.bottom-trash').addClass('bottom-trash-active')
   },
   out: function(event, ui) {
-    console.log("out");
+    $('.bottom-trash').removeClass('bottom-trash-active')
   }
 });
 
@@ -265,12 +273,12 @@ var auditTask = function(taskEl) {
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
   // ensure it worked
-  console.log(date); 
+  //console.log(date); 
 
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
   // this should print out an object for the value of the date variable, but at 5:00pm of that date
-  console.log(time);
+  //console.log(time);
 
   // remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
@@ -282,7 +290,9 @@ var auditTask = function(taskEl) {
 
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
-  }  
+  }
+  
+  console.log(taskEl)
 };
 
 
